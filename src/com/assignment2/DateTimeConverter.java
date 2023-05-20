d9064a5ce35be163fd609236b76f7e4467648906
shashipanta <shashipanta57@gmail.com>
2023-05-21 01:03:03 +0545

    Assessment-2: LocalDateTime to UTC and UTC to LocalDateTime conversion

--- src/com/assignment2/DateTimeConverter.java
+++ src/com/assignment2/DateTimeConverter.java
@@ -1,52 +1,44 @@
 package com.assignment2;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.Set;
-import java.util.TimeZone;
 
 public class DateTimeConverter {
 	
-public static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 public static final ZoneId systemZoneId = ZoneId.systemDefault();
+public static final ZoneId UTCZoneId = ZoneId.of("UTC");
+private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 
 	
 	public static LocalDateTime toUTC(LocalDateTime localDateTime) {
 		
 		ZonedDateTime localZonedDateTime = localDateTime.atZone(systemZoneId);
-		ZonedDateTime utcZonedDateTime = localZonedDateTime.withZoneSameInstant(systemZoneId);
+		ZonedDateTime utcZonedDateTime = localZonedDateTime.withZoneSameInstant(UTCZoneId);
 		return utcZonedDateTime.toLocalDateTime();
 	}
 	
+	public static LocalDateTime toLocal(LocalDateTime utcDateTime) {
+		
+		ZonedDateTime utcZonedDateTime = utcDateTime.atZone(UTCZoneId);
+		ZonedDateTime localZonedDateTime = utcZonedDateTime.withZoneSameInstant(systemZoneId);
+		return localZonedDateTime.toLocalDateTime();
+	}
+	
 	public static void main(String[] args) {
-//		Calendar
 		
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		LocalDateTime ldt = LocalDateTime.now();
+		LocalDateTime utcLocalDateTime = LocalDateTime.now(UTCZoneId);
+
+		System.out.println("Local Date Time      : " + ldt.format(formatter));
+		System.out.println("Into UTC Date Time   : " + toUTC(ldt).format(formatter));
 		
-		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_DATE_TIME;
+		System.out.println();
+
+		System.out.println("UTC Date Time        : " + utcLocalDateTime.format(formatter));
+		System.out.println("Into Local Date Time : " + toLocal(utcLocalDateTime).format(formatter));
 		
-		simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-		LocalDateTime localDateTime = LocalDateTime.now();
-		
-		LocalDateTime utcDateTime = LocalDateTime.now(ZoneOffset.UTC);
-		ZonedDateTime utcZonedDateTime = utcDateTime.atZone(ZoneOffset.UTC);
-		ZonedDateTime localZonedDateTime = utcZonedDateTime.withZoneSameInstant(ZoneId.systemDefault());
-		LocalDateTime currentTime = localZonedDateTime.toLocalDateTime();
-		
-		
-		System.out.println("UTC time : " + utcDateTime.format(dateTimeFormatter));
-		System.out.println("UTC Zone : " + utcZonedDateTime.format(dateTimeFormatter));
-		System.out.println("local Zone : " + localZonedDateTime.format(dateTimeFormatter));
-		System.out.println("Current Date time : " + currentTime);
-		
-		System.out.println("Local Time : " + localDateTime.toLocalDate() + localDateTime.toLocalTime());		
-		System.out.println("UTC time : " + toUTC(localDateTime));
 
 	}
 
